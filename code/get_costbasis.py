ac16078fab6e3c8cb8f7f1748e6735e623eee850
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-03-13 15:45:47 -0400

    Fix pair_sales()
    Use HIFO method

--- code/get_costbasis.py
+++ code/get_costbasis.py
@@ -42,37 +42,53 @@ def get_current_asset_amount(df):
 
 def pair_sales(df, rule='hifo', year=2021):
     df = df[df.Exchange != 'Fidelity']
-    sell_df = df[(df.Transaction == 'SELL') & (df.Date >= f'{year}-01-01') & (df.Date <= f'{year}-12-31')]
+    sale_in_year = [0 <= (date - pd.Timestamp(f'{year}-01-01 00:00:00+0000')).days <= 365 for date in df['Date']]
+    sell_df = df[(df.Transaction == 'SELL') & sale_in_year]
 
     pairs = []
     for asset in pd.unique(sell_df.Asset):
         
-        for s, sale in sell_df[sell_df.Asset == asset].iterrows():
+        for s, sale in sell_df[sell_df.Asset == asset].sort_values(by='Asset Price', ascending=True).iterrows():
+
             buy_df = df[(df.Transaction == 'BUY') & (df.Asset == asset)].loc[:s].sort_values(by='Asset Price', ascending=False) 
             
             for b, buy in buy_df.iterrows():
                 
-                diff = buy['Asset Amount'] - sale['Asset Amount']
-                if (diff > 0): # Is the highest buy enough to cover this sale?
+                diff = buy['Asset Amount'] - sale['Asset Amount'] # Is the highest buy enough to cover this sale?
+                if (diff > 0): # Yes
                     
-                    buy['Asset Amount'] = sale['Asset Amount']
+                    #buy['Asset Amount'] = sale['Asset Amount']
+                    amt = sale['Asset Amount']
+                    buy['Cost Basis'] = buy['Asset Price'] * amt
+                    sale['Cost Basis'] = sale['Asset Price'] * amt
+                    buy['Fee'] = buy['Fee'] * (amt/buy['Asset Amount'])
+                    buy['Asset Amount'] = amt
                     pairs.append(list(sale) + list(buy))
 
                     df.loc[b, 'Asset Amount'] = diff # Discount sell from buy amt in transaction record
+                    df.loc[b, 'Fee'] = df.loc[b, 'Fee'] - buy['Fee']
                     break # Go to next sale
 
-                elif (diff <= 0):
+                elif (diff <= 0): # No
                     
-                    sale['Asset Amount'] = buy['Asset Amount']
+                    amt = buy['Asset Amount']
+                    buy['Cost Basis'] = buy['Asset Price'] * amt
+                    sale['Cost Basis'] = sale['Asset Price'] * amt
+                    
+                    fee = sale['Fee']
+                    sale['Fee'] = fee * (amt/sale['Asset Amount'])
+                    sale['Asset Amount'] = amt
                     pairs.append(list(sale) + list(buy))
-                    
+
                     df = df.drop(b)
                     buy_df = buy_df.drop(b)
                     sale['Asset Amount'] = abs(diff)
+                    sale['Fee'] = fee - sale['Fee']
 
     pairs_df = pd.DataFrame.from_records(pairs)
     pairs_df.columns = [col + ' (Sell)' for col in list(df.columns)] + [col + ' (Buy)' for col in list(df.columns)]
 
-    pairs_df['Cost Basis (Sell)'] = pairs_df['Asset Price (Sell)'] * pairs_df['Asset Amount (Sell)']
-    pairs_df['Cost Basis (Buy)'] = pairs_df['Asset Price (Buy)'] * pairs_df['Asset Amount (Buy)']
+    pairs_df['Date (Sell)'] = [ts.strftime('%m-%d-%Y') for ts in pairs_df['Date (Sell)']]
+    pairs_df['Date (Buy)'] = [ts.strftime('%m-%d-%Y') for ts in pairs_df['Date (Buy)']]
+
     return pairs_df
